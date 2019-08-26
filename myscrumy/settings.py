de00698d167d7530583559e3899275d2c9a9582f
Daud <daudonmail@gmail.com>
2019-08-26 15:02:24 +0100

    config db

--- myscrumy/settings.py
+++ myscrumy/settings.py
@@ -80,10 +80,22 @@ WSGI_APPLICATION = 'myscrumy.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'myscrumy',
+        'USER':'root',
+        'PASSWORD':'Oladipo@2019',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
+        'OPTIONS': {
+          'autocommit': True,
+        }
     }
+
 }
 
 
