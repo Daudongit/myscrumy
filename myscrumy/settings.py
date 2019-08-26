a63e6a9e6adbbace3527de1d30f5d338c829795a
Daud <daudonmail@gmail.com>
2019-08-26 16:07:41 +0100

    reset db

--- myscrumy/settings.py
+++ myscrumy/settings.py
@@ -84,28 +84,28 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
-    # 'default': {
-    #     'ENGINE': 'mysql.connector.django',
-    #     'NAME': 'myscrumy',
-    #     'USER':'root',
-    #     'PASSWORD':'Oladipo@2019',
-    #     'HOST':'127.0.0.1',
-    #     'PORT':'3306',
-    #     'OPTIONS': {
-    #       'autocommit': True,
-    #     }
-    # }
     'default': {
         'ENGINE': 'mysql.connector.django',
-        'NAME': 'django',
+        'NAME': 'myscrumy',
         'USER':'root',
-        'PASSWORD':'',
+        'PASSWORD':'Oladipo@2019',
         'HOST':'127.0.0.1',
         'PORT':'3306',
         'OPTIONS': {
           'autocommit': True,
         }
     }
+    # 'default': {
+    #     'ENGINE': 'mysql.connector.django',
+    #     'NAME': 'django',
+    #     'USER':'root',
+    #     'PASSWORD':'',
+    #     'HOST':'127.0.0.1',
+    #     'PORT':'3306',
+    #     'OPTIONS': {
+    #       'autocommit': True,
+    #     }
+    # }
 
 }
 
